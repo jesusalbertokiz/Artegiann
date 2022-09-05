98e7fa0ea0ce3cbfb859678102907227738d45fc
jmxs <inversionesjmxs@gmail.com>
2022-09-05 19:11:09 -0400

    Mejore el diseño del foteer y lo hice mas responsive. Recurso usado https://github.com/itskrey/KreyAcademy/tree/main/Footer%20Responsive

--- src/components/elements/footer.elements.js
+++ src/components/elements/footer.elements.js
@@ -3,27 +3,69 @@ import { StaticImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
+  height: fit-content;
   width: 100%;
-  height: auto;
-  display: grid;
-  grid-template-columns: 40% auto auto auto;
-  grid-wrap: wrap;
-  gap: 1rem;
-  background-color: #330033;
+  padding: 3rem 6rem;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  @media (max-width: 768px) {
+    padding: 2rem;
+    flex-direction: column;
+  }
 `;
 
-export const FooterSecction = styled.div`
-  padding: 1rem;
+export const FooterContent = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 2rem 0;
+    border-bottom: 1px solid #adb9c7;
+    justify-content: center;
+    align-items: center;
+  }
 `;
+
+export const FooterMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 40%;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+`;
+
+export const FooterCopy = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid #adb9c7;
+`;
+
+// export const FooterSecction = styled.div`
+//   padding: 1rem;
+// `;
 
 export const FooterImage = styled(StaticImage)`
   width: 150px;
+  margin-bottom: 1.2rem;
 `;
 
 export const FooterLink = styled(Link)`
   font-family: "Rubik", sans-serif;
   font-weight: 200;
-  font-size: 70%;
   color: #adb9c7;
   &:hover {
     color: #fff;
@@ -33,6 +75,8 @@ export const FooterLink = styled(Link)`
 export const FooterItemSocial = styled.div`
   width: 100%;
   display: flex;
+  justify-content: center;
+  align-items: center;
   gap: 1rem;
   color: #adb9c7;
   padding-top: 1rem;
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,53 +3,41 @@ import { StaticImage } from "gatsby-plugin-image";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import {
   FooterContainer,
+  FooterContent,
+  FooterMenu,
+  FooterCopy,
   FooterItemSocial,
   FooterLink,
-  FooterSecction,
 } from "./elements/footer.elements";
 
 const Footer = () => {
   return (
     <footer>
       <FooterContainer>
-        <FooterSecction>
+        <FooterContent>
           <StaticImage
             imgClassName="logo"
             alt="Logo de la pagina"
             src="../images/logo.png"
             width={150}
           />
-          <p>
-            Giannella, es una joven de 16 años, nacida en Valencia Venezuela,
-            desde niña le ha gustado el mundo de las artes, desarrollando la
-            pintura, el dibujo, esculpe en plastilina, diseña, escribe cuentos,
-            es emprendedora, creo su propia marca de diseños exclusivos
-            ArteGiann y también es creadora digital de NFTs.
-          </p>
-          <p>
-            <br />© {new Date().getFullYear()} &middot;
-            {` `}
-            <FooterLink to="/">ArteGiann</FooterLink>
-          </p>
-        </FooterSecction>
-        <FooterSecction>
-          <h4>Recursos</h4>
-          <FooterLink to="/blog">Blog</FooterLink>
-          <br />
-          <FooterLink to="/discover">Descubrir</FooterLink>
-        </FooterSecction>
-        <FooterSecction>
-          <h4>Artista</h4>
-          <FooterLink to="/about-me">Sobre MI</FooterLink>
-          <br />
-          <FooterLink to="/contact">Contactame</FooterLink>
-        </FooterSecction>
-        <FooterSecction>
+          <span className="footer-info">111 111 111</span>
+          <span className="footer-info">academy@itskrey.com</span>
+        </FooterContent>
+        <FooterMenu>
+          <FooterContent>
+            <h4>Recursos</h4>
+            <FooterLink to="/blog">Blog</FooterLink>
+            <FooterLink to="/discover">Descubrir</FooterLink>
+          </FooterContent>
+          <FooterContent>
+            <h4>Artista</h4>
+            <FooterLink to="/about-me">Sobre MI</FooterLink>
+            <FooterLink to="/contact">Contactame</FooterLink>
+          </FooterContent>
+        </FooterMenu>
+        <FooterContent className="footer-content">
           <h4>Contácto</h4>
-          <p>
-            Puedes seguirme en <br />
-            mis redes sociales
-          </p>
           <FooterItemSocial>
             <a>
               <FaFacebook />
@@ -61,8 +49,15 @@ const Footer = () => {
               <FaTwitter />
             </a>
           </FooterItemSocial>
-        </FooterSecction>
+        </FooterContent>
       </FooterContainer>
+      <FooterCopy>
+        <span className="copyright">
+          © {new Date().getFullYear()} &middot;
+          {` `}
+          <FooterLink to="/">ArteGiann</FooterLink>
+        </span>
+      </FooterCopy>
     </footer>
   );
 };
